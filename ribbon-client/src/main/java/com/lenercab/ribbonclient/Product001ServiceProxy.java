b31a1ba22f9d62b656abaa5d218f912c4b5c891e
Leehener Cabeza <lcabeza@cobiscorp.com>
2019-11-06 15:58:17 -0500

    implementation zuul and ribbon

--- ribbon-client/src/main/java/com/lenercab/ribbonclient/Product001ServiceProxy.java
+++ ribbon-client/src/main/java/com/lenercab/ribbonclient/Product001ServiceProxy.java
@@ -11,17 +11,21 @@ import javax.validation.Valid;
 
 //@RibbonClient(name="product001",  configuration = RibbonConfiguration.class)
 //@FeignClient(name="product001", url = "localhost:8081")
-@FeignClient(name="product001")
+//@FeignClient(name="product001")
+@FeignClient(name="zuul")
 @RibbonClient(name="product001")
 public interface Product001ServiceProxy {
 
 
-    @GetMapping("/Product")
+//    @GetMapping("/Product")
+    @GetMapping("/product001/Product")
     public ResponseEntity<Object> GetProductAll();
 
-    @GetMapping("/Product/{id}")
+//    @GetMapping("/Product/{id}")
+    @GetMapping("/product001/Product/{id}")
     public ResponseEntity<Object> GetProductById(@PathVariable("id") Long id);
 
-    @PostMapping("/Product")
+//    @PostMapping("/Product")
+    @PostMapping("/product001/Product")
     public ResponseEntity<Object> saveProduct(@Valid @RequestBody Product product);
 }
